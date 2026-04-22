f9389a1bf5476b28999d8ffba1776740c650a633
Oscar Lizandro Vasquez Llave <vasquezllaveoscarlizandro@gmail.com>
2026-04-21 20:43:16 -0500

    feat(faq): add information to faq

--- src/script.js
+++ src/script.js
@@ -36,3 +36,21 @@ document.addEventListener('DOMContentLoaded', () => {
     // Carga inicial
     updateCards();
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const faqQuestions = document.querySelectorAll('.faq-question');
+
+    faqQuestions.forEach(question => {
+        question.addEventListener('click', () => {
+            const faqItem = question.parentElement;
+            
+            document.querySelectorAll('.faq-item').forEach(item => {
+                if (item !== faqItem) {
+                    item.classList.remove('active');
+                }
+            });
+
+            faqItem.classList.toggle('active');
+        });
+    });
+});
